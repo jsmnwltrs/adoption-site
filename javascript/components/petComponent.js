4395698d6de19900f1bdf85719dc7fa0e810f9af
Jasmine Walters <jsmnwltrs@gmail.com>
2018-10-03 20:32:11 -0500

    filter works

--- javascript/components/petComponent.js
+++ javascript/components/petComponent.js
@@ -10,21 +10,43 @@ const getPetz = () => {
     return pets;
 };
 
-const petBuilder = () => {
+const petBuilder = (petsArray) => {
     let domString = '';
-    for (let i = 0; i < pets.length; i ++){
+    for (let i = 0; i < petsArray.length; i ++){
         domString += 
-            `<div class="card" style="width: 18rem;">
-            <h5 class="card-title">${pets[i].name}</h5>
-            <img class="card-img" src="${pets[i].imageUrl}" alt="Card image cap">
+            `<div class="card ${petsArray[i].type}" style="width: 18rem;">
+            <h5 class="card-title">${petsArray[i].name}</h5>
+            <img class="card-img" src="${petsArray[i].imageUrl}" alt="Card image cap">
             <div class="card-body">
-                <p class="card-text">Color: ${pets[i].color}</p>
-                <p class="card-text">Special Skill: ${pets[i].specialSkill}</p>
-                <p class="card-text">Type: ${pets[i].type}</p>
+                <p class="card-text">Color: ${petsArray[i].color}</p>
+                <p class="card-text">Special Skill: ${petsArray[i].specialSkill}</p>
+                <p class="card-text">Type: ${petsArray[i].type}</p>
             </div>
           </div>`;
     };
     printToDom(domString, 'pets');
 }
 
-export {setPets, getPetz, petBuilder};
+
+const sortPets = (e) => {
+    const type = e.target.id;
+    if(type === 'all'){
+        petBuilder(pets);
+    } else {
+    const filteredPets = pets.filter(x => x.type === type);
+        petBuilder(filteredPets);
+}};
+
+const sortEvents = () => {
+    const allButton = document.getElementById('all');
+    const catButton = document.getElementById("cat");
+    const dogButton = document.getElementById("dog");
+    const dinoButton = document.getElementById("dino");
+    allButton.addEventListener("click", sortPets);
+    catButton.addEventListener("click", sortPets);
+    dogButton.addEventListener("click", sortPets);
+    dinoButton.addEventListener("click", sortPets);
+};
+
+
+export {setPets, getPetz, petBuilder, sortEvents};
